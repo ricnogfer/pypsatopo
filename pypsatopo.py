8ccbf436e298b41ec452b850ca0e0d826692b171
Ricardo <ricardo.fernandes@mpe.au.dk>
2025-02-07 08:09:11 +0100

    Bumped release version to 1.1.1

--- pypsatopo.py
+++ pypsatopo.py
@@ -3,12 +3,12 @@
 
 
 __project__ = "PyPSATopo"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __description__ = "PyPSATopo is a tool that allows generating the topographical representation of any arbitrary PyPSA-based network"
 __license__ = "BSD 3-Clause"
 __author__ = "Energy Systems Group at Aarhus University (Denmark)"
 __contact__ = "ricardo.fernandes@mpe.au.dk"
-__status__ = "Production"
+__status__ = "Development"
 
 
 
